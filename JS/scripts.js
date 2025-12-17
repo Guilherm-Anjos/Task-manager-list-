9332b9cdea1552c8a9f51e76938e2e8959861465
Guilherme Anjos <guilherme.r.dos.anjos@outlook.com>
2025-12-16 23:49:32 -0300

    Terminando de configurar os botões.

--- JS/scripts.js
+++ JS/scripts.js
@@ -9,7 +9,36 @@ const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
 //Funções
 
+const saveTodo = (Text) => {
+    const todo = document.createElement("div")
+        todo.classList.add("todo")
 
+        const todoTitle = document.createElement ("h3")
+            todoTitle.innerText = Text
+            todo.appendChild(todoTitle);
+
+            const doneBtn = document.createElement("button")
+                doneBtn.classList.add("finish todo")
+            doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+        todo.appendChild(doneBtn)     
+
+            const editBtn = document.createElement("button")
+                editBtn.classList.add("edit-todo")
+            editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
+        todo.appendChild(editBtn)     
+
+            const deleteBtn = document.createElement("button")
+                deleteBtn.classList.add("remove-todo")
+            deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+        todo.appendChild(deleteBtn)
+
+        todoList.appendChild(todo);
+
+
+        todoInput.Value = "";
+        todoInput.focus();
+    
+}
 
 //Eventos
 
@@ -17,5 +46,12 @@ todoForm.addEventListener("submit", (e) =>{
     e.preventDefault();
 
     console.log("Enviou form");
-    
+
+    const inputValue = todoInput.Value
+    if(inputValue){
+        console.log(inputValue)
+        {
+        saveTodo(inputValue)}
+    }
+
 })
